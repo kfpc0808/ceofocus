bd5cb0851aedc9f5b6a8fabedcc617632a38e0b4
kfpc0808 <kfp_center@naver.com>
2025-10-12 22:01:49 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,16 @@
+// Firebase 설정
 const firebaseConfig = {
-  apiKey: "AIzaSyDburefZCVqCY8QQppcdQFoqVFpMriv1m0",
+  apiKey: "AIzaSyDbufefZCVqCY8QQppcdQFoqVFpMriv1m0",
   authDomain: "kfpc-company-support-project.firebaseapp.com",
   databaseURL: "https://kfpc-company-support-project-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "kfpc-company-support-project",
   storageBucket: "kfpc-company-support-project.firebasestorage.app",
-  messagingSenderId: "101260933373",
-  appId: "1:101260933373:web:3436176f9ca256005​6d914",
+  messagingSenderId: "1012609333373",
+  appId: "1:1012609333373:web:3436176f9ca2560056d914",
   measurementId: "G-TRQCDPL8B4"
 };
 
 // Firebase 초기화
 firebase.initializeApp(firebaseConfig);
-
-// 내보내기
 const auth = firebase.auth();
 const db = firebase.firestore();
